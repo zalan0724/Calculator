ca36722f31e85901a7684dc5278c3a83a3fecbc1
zalan0724 <munkacsi0724@gmail.com>
2021-02-07 11:10:56 +0100

    Finishing first version

--- script.js
+++ script.js
@@ -1,78 +1,235 @@
 let firstNumber = '';
 let secondNumber = '';
-let result = '';
-let operationType = '';
-let equalType = '';
-let isOperationPressed= false;
+let operatorType = '';
+let equalSign = '';
+let result = 0;
+let factorialSign = '';
+let operators = ['+','-','*','/','%'];
+
+//Switches
+let isFirstNumber = true;
+let isfirstDigit1 = true;
+let isfirstDigit2 = true;
+let canBeFloat = false;
+let canBeOperator = false;
+let canBeFactorial = false;
+let canBeEqual = false;
+let finished = false;
+
 let numberButtons = document.querySelectorAll('.numberButton');
 let operatorButtons = document.querySelectorAll('.operator');
+let calculatingLine = document.querySelector('#calculating');
+let resultLine = document.querySelector('#result');
+let floatButton = document.querySelector('#float');
 let equalButton = document.querySelector('#equal');
-let calculatingDisplay = document.querySelector('#calculating');
-let resultDisplay = document.querySelector('#result');
-let operators = ['+','-','/','*'];
+let factorialButton = document.querySelector('#factorial');
+let clearEntryButton = document.querySelector('#clearEntry');
+let clearButton = document.querySelector('#clear');
 
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
-}
+  }
 
+//Operations
 function add(){
-    return parseInt(firstNumber)+parseInt(secondNumber);
+    return Number(firstNumber) + Number(secondNumber);
 }
 function subtrack(){
-    return parseInt(firstNumber)-parseInt(secondNumber);
+    return Number(firstNumber) - Number(secondNumber);
 }
 function multiply(){
-    return parseInt(firstNumber)*parseInt(secondNumber);
+    return Number(firstNumber) * Number(secondNumber);
 }
 function divide(){
-    return parseInt(firstNumber)/parseInt(secondNumber);
+    return Number(firstNumber) / Number(secondNumber);
 }
-
-function updateDisplay(){
-    calculatingDisplay.innerHTML = firstNumber + ' ' + operationType + ' ' + secondNumber + ' ' + equalType;
-    resultDisplay.innerHTML = result;
+function modulo(){
+    return Number(firstNumber) % Number(secondNumber);
 }
-
-function equal(){
-    switch(operationType){
-        case '+':
-            result = add()
-            break;
-        case '-':
-            result = subtrack();
-            break;
-        case '*':
-            result = multiply();
-            break;
-        case '/':
-            result = divide();
-            break;
+function factorial(){
+    let resultTemp = 1;
+    for(let i =1; i<=Number(firstNumber);i++){
+        resultTemp*=i;
     }
-    updateDisplay();
-    console.log('szai');
+    result=resultTemp;
+    factorialSign='!';
+    equalSign='=';
+    finished = true;
 }
 
-function reset(){
-    
+//Main functions
+
+function clearEntry(){
+    firstNumber = '';
+    secondNumber = '';
+    operatorType = '';
+    equalSign = '';
+    result = 0;
+    factorialSign = '';
+
+    isFirstNumber = true;
+    isfirstDigit1 = true;
+    isfirstDigit2 = true;
+    canBeFloat = false;
+    canBeOperator = false;
+    canBeEqual = false;
+    canBeFactorial = false;
+    finished = false;
+
+    updateDisplay();
 }
-function typing(value){
-    //First Number
-    if(!isOperationPressed){
-        //Not operator
-        if(!(operators.includes(value))){
-            firstNumber+=value;
-        }
-        //Operator
-        else{
-            operationType+=value;
-            isOperationPressed=true;
+
+function clear(){
+    //Finished
+    if(finished){
+        result = 0;
+        finished = false;
+        equalSign = '';
+
+        if(factorialSign==='!') canBeFactorial = true;
+        factorialSign = '';
+
+        if(secondNumber.charAt(secondNumber.length-1)==='.') canBeFloat = true;
+
+        secondNumber=secondNumber.slice(0,-1);
+
+        if(secondNumber===''){
+            canBeFloat=false;
+            isfirstDigit2 = true;
+            canBeEqual = false;
         }
     }
     //Second Number
-    else if(isNumeric(value)) secondNumber+=value;
+    else if(!isFirstNumber && secondNumber!==''){
+        if(secondNumber.charAt(secondNumber.length-1)==='.') canBeFloat = true;
 
+        secondNumber=secondNumber.slice(0,-1);
+
+        if(secondNumber===''){
+            canBeFloat=false;
+            isfirstDigit2 = true;
+            canBeEqual = false;
+        }
+    }
+    //Operator
+    else if(!isFirstNumber && secondNumber===''){
+        operatorType = '';
+        isFirstNumber = true;
+        canBeOperator = true;
+        canBeFactorial = true;
+    }
+    //First Number
+    else if(isFirstNumber && firstNumber!==''){
+        if(firstNumber.charAt(firstNumber.length-1)==='.') canBeFloat = true;
+        
+        firstNumber = firstNumber.slice(0,-1);
+        if(firstNumber==='') clearEntry();
+    }
     updateDisplay();
 }
-numberButtons.forEach(element => element.addEventListener('click',() => typing(element.value)));
-operatorButtons.forEach(element => element.addEventListener('click',() => typing(element.value)));
-equalButton.addEventListener('click', equal);
+
+function updateDisplay(){
+    calculatingLine.innerHTML = firstNumber + factorialSign + ' ' + operatorType + ' ' + secondNumber + ' ' + equalSign;
+    resultLine.innerHTML = result;
+}
+
+function makeEqual(){
+    switch(operatorType){
+        case '+':
+            result = Math.round(Number(add())*1000)/1000;
+            break;
+        case '-':
+            result = Math.round(Number(subtrack())*1000)/1000;
+            break;
+        case '*':
+            result = Math.round(Number(multiply())*1000)/1000;
+            break;
+        case '/':
+            result = Math.round(Number(divide())*1000)/1000;
+            break;
+        case '%':
+            result = Math.round(Number(modulo())*1000)/1000;
+    }
+    equalButton.removeEventListener('click', ()=>typing(equalButton.value));
+    finished = true;
+    equalSign = '=';
+    updateDisplay();
+}
+
+function typing(inputValue){
+    if(!finished){
+        //First Number
+        if(isFirstNumber){
+            //Number
+            if(isNumeric(inputValue)){
+                if(isfirstDigit1){
+                    canBeFactorial = true;
+                    canBeOperator = true;
+                    canBeFloat = true;
+                    isfirstDigit1 = false;
+                }
+                firstNumber+=inputValue;
+            }
+            //Float
+            else if(inputValue==='.' && canBeFloat){
+                canBeFloat = false;
+                firstNumber+=inputValue;
+            }
+            //Factorial
+            else if(inputValue==='!' && canBeFactorial){
+                canBeFactorial = false;
+                factorial();
+            }
+            //Operator
+            else if(operators.includes(inputValue) && canBeOperator){
+                canBeOperator = false;
+                isFirstNumber = false;
+                operatorType = inputValue;
+            }
+        }
+        //Second Number
+        else if(!isFirstNumber){
+            if(isNumeric(inputValue)){
+                if(isfirstDigit2){
+                    canBeFloat = true;
+                    canBeEqual = true;
+                    isfirstDigit2 = false;
+                }
+                secondNumber+=inputValue;
+            }
+            else if(inputValue==='.' && canBeFloat){
+                canBeFloat = false;
+                secondNumber+=inputValue;
+            }
+            else if(inputValue==='=' && canBeEqual) makeEqual();
+        }
+    }
+    else if(finished){
+        if(isNumeric(inputValue)){
+            clearEntry();
+            canBeFactorial = true;
+            canBeOperator = true;
+            canBeFloat = true;
+            isfirstDigit1 = false;
+            firstNumber+=inputValue;
+        }
+        else if(operators.includes(inputValue)){
+            let resultBackUp = result;
+            clearEntry();
+            firstNumber = resultBackUp;
+            operatorType = inputValue;
+            isFirstNumber = false; 
+            isfirstDigit1 = false;
+        }
+    }
+    else console.log('Van baj szatyorral :(');
+    updateDisplay();
+}
+//Adding eventListeners
+numberButtons.forEach(element => element.addEventListener('click',()=>typing(element.value)));
+operatorButtons.forEach(element => element.addEventListener('click', () => typing(element.value)));
+floatButton.addEventListener('click', () => typing(floatButton.value));
+factorialButton.addEventListener('click', () => typing(factorialButton.value));
+equalButton.addEventListener('click', ()=>typing(equalButton.value));
+clearEntryButton.addEventListener('click',clearEntry);
+clearButton.addEventListener('click',clear);
